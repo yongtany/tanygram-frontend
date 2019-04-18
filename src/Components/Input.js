f4bdc53c1a8bf31217dccebdd2225d4f9f57cb72
yongtany <yongn1494@gmail.com>
2019-04-19 00:24:52 +0900

    Auth from with Hooks

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -12,9 +12,19 @@ const Container = styled.input`
   padding: 0 15px;
 `;
 
-const Input = ({placeholder}) => <Container placeholder={placeholder}/>;
+const Input = ({placeholder, required = true, value, onChange, type="text" }) => (
+  <Container
+    placeholder={placeholder}
+    required={required}
+    value={value}
+    onChange={onChange}
+  />
+);
 
 Input.propTypes = {
-  placeholder: PropTypes.string.isRequired
+  placeholder: PropTypes.string.isRequired,
+  required: PropTypes.bool,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired
 }
 export default Input;
