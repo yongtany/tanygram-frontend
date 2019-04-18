61c923ad077480ec3d918ea3029021bba4eebd5e
yongtany <yongn1494@gmail.com>
2019-04-18 16:36:06 +0900

    GlobalStyles and Theme

---
+++ src/Styles/Theme.js
@@ -0,0 +1,18 @@
+const BORDER_RADIUS = "1px solid #e6e6e6";
+const BOX_BORDER = "4px";
+
+export default {
+  bgColor: "#FAFAFA",
+  blackColor: "#262626",
+  darkGreyColor: "#999",
+  lightGreyColor: "#c7c7c7",
+  redColor: "#ED4956",
+  blueColor: "#3897f0",
+  darkBlueColor: "#003569",
+  boxBorder: "1px solid #e6e6e6",
+  borderRadius: "4px",
+  whiteBox: `${BORDER_RADIUS};
+             ${BOX_BORDER};
+             background-color:white;
+  `
+}
